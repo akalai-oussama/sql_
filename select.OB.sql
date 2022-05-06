769273734857c9fac9663717fe0525d7b42eb931
oussamaa koz <oussamaakalay76@gmail.com>
2022-05-06 09:09:53 +0000

    select objet

---
+++ select.OB.sql
@@ -0,0 +1,10 @@
+SELECT * FROM aliment WHERE calories < 90;
+
+
+SELECT * FROM aliment WHERE nom = “poire”;
+
+
+SELECT * FROM aliment WHERE id = 4;
+
+
+SELECT * FROM utilisateur WHERE email LIKE “%gmail.com”;
